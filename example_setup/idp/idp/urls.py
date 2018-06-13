7528e2522756c5af8eceda51026649e5b0f22b44
Matt Saunders <m@tt-s.co.uk>
2018-06-13 14:12:34 +0100

    Changes to example idp: Use namespaced url Enabled admin and static for easy user management and customisation Extended validUntil on sp_metadata.xml

--- example_setup/idp/idp/urls.py
+++ example_setup/idp/idp/urls.py
@@ -1,11 +1,15 @@
 from django.urls import include, path
 from django.contrib import admin
 from django.contrib.auth.views import login
+from django.conf import settings
+from django.conf.urls.static import static
 
 import djangosaml2idp
 
+app_name = 'example_idp'
 urlpatterns = [
     #path('idp/', include('djangosaml2idp.urls')),
-    path('idp/', include('djangosaml2idp.urls')),
+    path('idp/', include('djangosaml2idp.urls', namespace='djangosaml2')),
     path('login/', login, {'template_name': 'idp/login.html'}, name='login'),
-]
+    path('admin/', admin.site.urls),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
