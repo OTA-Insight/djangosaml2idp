538442bbb11d37c1c5d6b408fc65238e114d19fb
Alexander Skvortsov <askvortsov@youthneuro.org>
2019-03-13 08:50:06 -0400

    Added some more tests

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -23,3 +23,38 @@ class TestBaseProcessor:
         user.first_name = 'test_first_name'
 
         assert BaseProcessor('entity-id').get_user_id(user) == 'test_first_name'
+
+    def test_extract_user_id_from_sp_config(self):
+
+        user = User()
+        user.special_id = 'test_special_id'
+
+        sp_config = {
+            'nameid_field': 'special_id'
+        }
+
+        assert BaseProcessor('entity_id').get_user_id(user, sp_config) == 'test_special_id'
+
+    def test_identity_dict_creation(self):
+
+        user = User()
+        user.name = 'Test Name'
+        user.email = 'test@email.com'
+        user.other_setting = 'Test Setting'
+        user.setting_not_passed = 'Test Setting Not Passed'
+
+        sp_config = {
+            'attribute_mapping': {
+                'name': 'fullName',
+                'email': 'emailAddress',
+                'other_setting': 'otherSetting'
+            }
+        }
+
+        expected_result = {
+            'fullName': 'Test Name',
+            'emailAddress': 'test@email.com',
+            'otherSetting': 'Test Setting'
+        }
+
+        assert BaseProcessor('entity_id').create_identity(user, sp_config) == expected_result
