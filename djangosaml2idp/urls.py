86d1cb5bb98bb91d8b98230a18fcff483b7beea6
Vijith <vijith@instamojo.com>
2019-06-19 11:46:40 +0530

    more url fixes + template name fix

--- djangosaml2idp/urls.py
+++ djangosaml2idp/urls.py
@@ -7,14 +7,14 @@ from . import views
 app_name = 'djangosaml2idp'
 
 urlpatterns = [
-    url(r'^login/', views.LoginAuthView.as_view(), name='login'),
+    url(r'^login/$', views.LoginAuthView.as_view(), name='login'),
     url(r'^sso/init', views.SSOInitView.as_view(), name="saml_idp_init"),
     url(r'^sso/(?P<binding>[a-zA-Z]+)', views.sso_entry, name="saml_login_binding"),
     url(r'^login/process/$', views.LoginProcessView.as_view(),
         name='saml_login_process'),
     url(r'^login/process_multi_factor/$', views.get_metadata,
         name='saml_multi_factor'),
-    url(r'^login/process_user_agreement/', views.UserAgreementScreen.as_view(),
+    url(r'^login/process_user_agreement/$', views.UserAgreementScreen.as_view(),
         name='saml_user_agreement'),
     url(r'^slo/(?P<binding>[a-zA-Z]+)', views.LogoutProcessView.as_view(),
         name="saml_logout_binding"),
