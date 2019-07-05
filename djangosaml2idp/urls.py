cf635511abeb8ab81bcb8257f785c7c44d87fe00
Vijith <vijith@instamojo.com>
2019-07-05 10:05:58 +0530

    disabling some unused urls

--- djangosaml2idp/urls.py
+++ djangosaml2idp/urls.py
@@ -1,4 +1,5 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 
 from django.conf.urls import url
 
@@ -7,15 +8,15 @@ from . import views
 app_name = 'djangosaml2idp'
 
 urlpatterns = [
-    url(r'^login/$', views.LoginAuthView.as_view(), name='login'),
-    url(r'^sso/init', views.SSOInitView.as_view(), name="saml_idp_init"),
+    # url(r'^login/$', views.LoginAuthView.as_view(), name='login'),
+    # url(r'^sso/init', views.SSOInitView.as_view(), name="saml_idp_init"),
     url(r'^sso/(?P<binding>[a-zA-Z]+)/$', views.sso_entry, name="saml_login_binding"),
     url(r'^login/process/$', views.LoginProcessView.as_view(),
         name='saml_login_process'),
-    url(r'^login/process_multi_factor/$', views.get_metadata,
-        name='saml_multi_factor'),
-    url(r'^login/process_user_agreement/$', views.UserAgreementScreen.as_view(),
-        name='saml_user_agreement'),
+    # url(r'^login/process_multi_factor/$', views.get_metadata,
+    #     name='saml_multi_factor'),
+    # url(r'^login/process_user_agreement/$', views.UserAgreementScreen.as_view(),
+    #     name='saml_user_agreement'),
     url(r'^slo/(?P<binding>[a-zA-Z]+)', views.LogoutProcessView.as_view(),
         name="saml_logout_binding"),
     url(r'^metadata/$', views.metadata, name='saml2_idp_metadata'),
