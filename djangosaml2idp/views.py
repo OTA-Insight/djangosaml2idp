3d53deb137c6f48d675ed9fb76d543bd5b0dcc35
Michiel Holtkamp <michiel@elements.nl>
2020-02-03 13:46:51 +0100

    Bugfix render_login_html_to_string (refs: #53)

--- djangosaml2idp/views.py
+++ djangosaml2idp/views.py
@@ -184,10 +184,10 @@ class IdPHandlerViewMixin:
         )
         return authn_resp
 
-    def render_login_html_to_string(self, template_name, context=None, request=None, using=None):
+    def render_login_html_to_string(self, context=None, request=None, using=None):
         """ Render the html response for the login action. Can be using a custom html template if set on the view. """
         default_login_template_name = 'djangosaml2idp/login.html'
-        custom_login_template_name = getattr(self, 'login_html_template')
+        custom_login_template_name = getattr(self, 'login_html_template', None)
         if custom_login_template_name:
             try:
                 template = get_template(custom_login_template_name, using=using)
