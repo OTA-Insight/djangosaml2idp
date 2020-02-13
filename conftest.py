2203f92b3d86f62f7771ea9393f194802a6ddbe2
Adam Mertz <adammertz@gmail.com>
2020-02-13 09:46:54 -0600

    cache example XML strucutre fixture result

--- conftest.py
+++ conftest.py
@@ -1,9 +1,12 @@
+from functools import lru_cache
+
 import pytest
 
 FILE_PREFIX = "tests/"
 
 
 @pytest.fixture()
+@lru_cache()
 def sample_saml_minimal():
     with open(FILE_PREFIX + "xml/min/request/sample_saml_request_minimal.xml") as f:
         expected_result = f.readline()
