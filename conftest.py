ccd2d9c70a1743b9ac73eddc8fc81efef2a57422
Adam Mertz <adammertz@gmail.com>
2020-02-13 09:45:03 -0600

    Drop unnecessary fixture

--- conftest.py
+++ conftest.py
@@ -1,5 +1,3 @@
-import xml
-
 import pytest
 
 FILE_PREFIX = "tests/"
@@ -10,8 +8,3 @@ def sample_saml_minimal():
     with open(FILE_PREFIX + "xml/min/request/sample_saml_request_minimal.xml") as f:
         expected_result = f.readline()
     return expected_result
-
-
-@pytest.fixture()
-def sample_saml_minimal_pretty(sample_saml_minimal):
-    return xml.dom.minidom.parseString(sample_saml_minimal).toprettyxml()
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,12 @@
+import xml
+
 from djangosaml2idp.utils import repr_saml, encode_saml
 
 
 # repr_saml and encode_saml are inverse functions. By testing them against each other, we test both.
 class TestSAMLEncodeAndDecode:
-    def test_with_minimal_saml_request_b64(self, sample_saml_minimal, sample_saml_minimal_pretty):
-        assert repr_saml(encode_saml(sample_saml_minimal), b64=True) == sample_saml_minimal_pretty
+    def test_with_minimal_saml_request_b64(self, sample_saml_minimal):
+        assert repr_saml(encode_saml(sample_saml_minimal), b64=True) == xml.dom.minidom.parseString(sample_saml_minimal).toprettyxml()
 
-    def test_with_internal_saml_response_zlib(self, sample_saml_minimal, sample_saml_minimal_pretty):
-        assert repr_saml(encode_saml(sample_saml_minimal, use_zlib=True)) == sample_saml_minimal_pretty
+    def test_with_internal_saml_response_zlib(self, sample_saml_minimal):
+        assert repr_saml(encode_saml(sample_saml_minimal, use_zlib=True)) == xml.dom.minidom.parseString(sample_saml_minimal).toprettyxml()
