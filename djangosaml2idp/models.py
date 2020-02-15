9d934e0e35bbaac756761ce9fcd431ce6fdd3c51
Alexander Skvortsov <38059171+askvortsov1@users.noreply.github.com>
2020-02-15 11:02:43 -0500

    Always all refresh_metadata on form submission (#73)
    Right now, refresh_metadata is only called when remote_metadata_url is present. This means that expiration datetimes aren't parsed out of locally provided metadata. Additionally, error logging has been added if metadata has expired but there's no remote url, and if metadata can't be fetched from remote

--- djangosaml2idp/models.py
+++ djangosaml2idp/models.py
@@ -48,7 +48,12 @@ class ServiceProvider(models.Model):
         '''
         if not self.local_metadata or not self.metadata_expiration_dt or now() > self.metadata_expiration_dt or force_refresh:
             if self.remote_metadata_url:
-                self.local_metadata = validate_metadata(fetch_metadata(self.remote_metadata_url))
+                try:
+                    self.local_metadata = validate_metadata(fetch_metadata(self.remote_metadata_url))
+                except Exception:
+                    logger.error(f'Metadata for SP {self.entity_id} could not be pulled from remote url {self.remote_metadata_url}.')
+            elif now() > self.metadata_expiration_dt:
+                logger.error(f'Metadata for SP {self.entity_id} has expired, no remote metadata found to refresh.')
             self.metadata_expiration_dt = extract_validuntil_from_metadata(self.local_metadata)
             return True
         return False
